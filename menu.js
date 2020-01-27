e3c91eee793dea69f2a26153da965d2481e07c0d
Babeth <labeau.elisabeth@neuf.fr>
2020-01-27 12:21:40 +0100

    modification index.html

--- menu.js
+++ menu.js
@@ -1,10 +1,15 @@
 const menu = `
 <ul>
-    <li>
-        <a href="index.html">acceuil</a>
-        <li>
-        <a href="contact.html">contact</a>
-    </li>
+    
+     
+    <li><a href="index.html">acceuil</a></li>
+      
+       
+    <li><a href="contact.html">contact</a></li>
+        
+    <li> <a href="presentation.html">contact</a></li>
+ 
+
 </ul>`;
 
 document.getElementById("navigation").innerHTML =menu;
