0345f0d0eb08447856634b702db76cadce0b8296
Babeth <labeau.elisabeth@neuf.fr>
2020-01-27 11:27:37 +0100

    premier module js menu

---
+++ menu.js
@@ -0,0 +1,10 @@
+cons menu = `
+<ul>
+    <li>
+        <a href="index.html">acceuil</a>
+        <li>
+        <a href="contact.html">contact</a>
+    </li>
+</ul>`;
+
+document.getElementById("navigation").innerHTML =menu;
